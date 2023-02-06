be2b4cab5c539ba70bbdb61bf66168a5d2a32a11
oh <cjdduf3@naver.com>
2023-02-06 11:37:03 +0900

    comp add childCop

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
   <mountedCompo></mountedCompo>
   <watchCompo></watchCompo>
+  <childCop v-bind:msg="greeting"></childCop>
 </template>
 
 <script>
 import mountedCompo from './components/mountedCompo.vue';
 import watchCompo from './components/watchCompo.vue';
+import childCop from './components/childCop.vue';
 
 export default {
   name: 'App',
   components: {
     mountedCompo,
-    watchCompo
+    watchCompo,
+    childCop
 
+  },
+  data(){
+    return{
+      greeting : '부모 컴포넌트로 부터 prop 전달'
+    }
   }
 }
 </script>
---
+++ src/components/childCop.vue
@@ -0,0 +1,16 @@
+<template >
+  {{ msg || 'prop가 아직 전달 되지 않음' }}
+</template>
+
+<script>
+export default {
+  props:{
+    msg: String
+  }
+
+}
+</script>
+
+<style>
+
+</style>
